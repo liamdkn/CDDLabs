1c1ec861bc531ebd06f8cc7f92a553305f14e64b
liamdkn <liamdurkan24@icloud.com>
2023-10-11 15:12:51 +0100

    barrier functioning

--- labTwo/rendezvous.cpp
+++ labTwo/rendezvous.cpp
@@ -4,27 +4,30 @@
 #include <iostream>
 
 int arrivedThreads = 0;
-Semaphore mutexSem(0);
-Semaphore barrierSem(1);
 
 /*! displays the first function in the barrier being executed */
 void task(std::shared_ptr<Semaphore> mutexSem,std::shared_ptr<Semaphore> barrierSem, int threadCount){
 
-  mutexSem->Wait(); //Enter the critical section
-  std::cout << "first" << std::endl;
+  mutexSem->Wait(); //Open the critical section
+  std::cout << "first" << std::endl;  //demonstrate threads completing task
+
   ++arrivedThreads;  //Increase arrived thread count
 
   if (arrivedThreads == threadCount) {
       //If all threads have arrived, signal the barrier semaphore to proceed
+      mutexSem->Signal(); //Release the mutual exclusion lock
       barrierSem->Signal();
   }
   else{
     //Not all threads have arrived at the first step
     mutexSem->Signal(); //Release the mutual exclusion lock
+    barrierSem->Wait(); //Wait for the barrier semaphore to proceed
   }
-  barrierSem->Wait(); //Wait for the barrier semaphore to proceed
-  
+
+  mutexSem->Wait(); //Open the critical section
   std::cout << "second" << std::endl;
+  mutexSem->Signal(); //Release critical section lock
+
   barrierSem->Signal(); 
 }
 
